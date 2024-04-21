74c013d524a0c0a57990fc52b07c43f9ba72eaa8
YenchiaChen <charlisyenchiachen@gmail.com>
2024-04-22 00:53:30 +0800

    style

--- src/Pages/LandingPage/index.tsx
+++ src/Pages/LandingPage/index.tsx
@@ -2,9 +2,9 @@ import img_bg from "../../assets/img/bg.png";
 import img_building from "../../assets/img/buildingonly.png";
 import img_tree from "../../assets/img/tree2.png";
 import img_bush from "../../assets/img/bush.png";
-import img_card01 from "../../assets/img/cards/1.png"
-import img_card02 from "../../assets/img/cards/2.png"
-import img_card03 from "../../assets/img/cards/3.png"
+import img_card01 from "../../assets/img/cards/1.png";
+import img_card02 from "../../assets/img/cards/2.png";
+import img_card03 from "../../assets/img/cards/3.png";
 import { motion, useScroll, useSpring, useTransform } from "framer-motion";
 import { useEffect, useRef, useState } from "react";
 
@@ -15,9 +15,9 @@ export default function LandingPage() {
     offset: ["start start", "end end"],
   });
 
-  const buildingY = useTransform(scrollYProgress, [0, 0.2], ["100px", "0px"]);
-  const treeY = useTransform(scrollYProgress, [0, 0.1], ["200px", "0px"]);
-  const bushY = useTransform(scrollYProgress, [0, 0.1], ["50px", "0px"]);
+  const buildingY = useTransform(scrollYProgress, [0, 0.2], ["80%", "10%"]);
+  const treeY = useTransform(scrollYProgress, [0, 0.1], ["200%", "0%"]);
+  const bushY = useTransform(scrollYProgress, [0, 0.1], ["100%", "56%"]);
   const physics = { damping: 15, mass: 0.9, stiffness: 50 };
   const buildingSpring = useSpring(buildingY, physics);
   const treespring = useSpring(treeY, physics);
@@ -34,7 +34,7 @@ export default function LandingPage() {
 
   return (
     <div ref={ref}>
-      <div className="h-[90vh] w-full bg-gradient-to-r from-[#19734E] to-[#3C9180] relative overflow-hidden rounded-full mt-[5vh] shadow-[inset_0_16px_24px_rgba(0,0,0,0.1)]">
+      <div className="w-full max-w-[2400px] mx-auto h-[90svh] max-h-[1000px] bg-gradient-to-r from-[#19734E] to-[#3C9180] relative overflow-hidden rounded-full mt-[5vh] shadow-[inset_0_16px_24px_rgba(0,0,0,0.1)]">
         <div className="w-fit text-white text-[14px] font-light flex justify-center flex-wrap gap-2 absolute left-1/2 -translate-x-1/2 top-8 items-center z-20">
           <p
             className="duration-300 cursor-pointer hover:bg-gray-700/20 px-4 py-2 rounded-full"
@@ -186,7 +186,7 @@ export default function LandingPage() {
           </div>
         </div>
 
-        <div className="flex flex-col justify-end h-[100%] container pl-12 mx-auto pb-36 text-white z-10 relative">
+        <div className="flex flex-col justify-center h-[100%]  container  mx-auto  text-white z-10 relative">
           <h1 className="text-[64px] font-semibold ">永續至上</h1>
           <p className="text-[20px] mt-4 font-light tracking-[2px]">
             致力環境保護發展，守護每位員工權益， <br />
@@ -196,63 +196,111 @@ export default function LandingPage() {
         <motion.img
           src={img_building}
           alt="imgBuilding"
-          className="absolute right-0 bottom-0 w-[800px]"
+          className="absolute -right-[10%] bottom-0 w-[60%]"
           style={{ y: buildingSpring }}
         />
         <motion.img
           src={img_tree}
           alt="imgTree"
-          className="absolute right-0 bottom-0 w-[600px]"
+          className="absolute -right-[5%] bottom-0 w-[55%]"
           style={{ y: treespring }}
         />
         <motion.img
           src={img_bush}
           alt="imgBush"
-          className="absolute right-[500px] -bottom-[230px] w-[600px]"
+          className="absolute right-[27%] -bottom-[29%] w-[45%]"
           style={{ y: bushSpring }}
         />
       </div>
 
-      <div className="h-[3000px] container mx-auto">
-        <p className="text-center mt-16 text-[36px]  text-[#555555] split-text font-serif">
-           相信深耕 共展未來 
-        </p>
+      <div className="container mx-auto mt-[10vh]">
+        <div className="grid grid-cols-3 items-center mt-16">
+          <div className='h-[2px] w-full bg-[#FFC759]'></div>
+          <p className="text-center  text-[36px]  text-[#555555] split-text font-serif">
+            相信深耕 共展未來
+          </p> 
+          <div className='h-[2px] w-full bg-[#FFC759]'></div>
+        </div>
         <div className="grid grid-cols-3 gap-8 mt-12">
-          <div className='relative group'>
-            <div className=" rounded-[30px]  py-8 px-8 h-[100%] glow-card relative overflow-hidden min-h-[240px] duration-300 hover:shadow-xl hover:scale-[1.04] z-10" style={{ backgroundImage: `url(${img_card01})`, backgroundSize: 'cover',  }}>
+          <div className="relative group">
+            <div
+              className=" rounded-[30px]  py-8 px-8 h-[100%] glow-card relative overflow-hidden min-h-[240px] duration-300 hover:shadow-xl hover:scale-[1.04] z-20"
+              style={{
+                backgroundImage: `url(${img_card01})`,
+                backgroundSize: "cover",
+              }}
+            >
               <div className="w-[100%] h-[100%] z-10 absolute bg-black/30 left-0 top-0 rounded-[30px]"></div>
-              <p className="absolute text-white z-20 font-serif text-[90px] -right-1 -bottom-4">在<br />地</p>
+              <p className="absolute text-white z-20 font-serif text-[90px] -right-1 -bottom-4">
+                在<br />地
+              </p>
             </div>
 
-            <div className='absolute shadow-xl w-full left-0 group-hover:pb-12 top-0 duration-300 group-hover:translate-y-[80%] pt-[20%] rounded-b-[30px] px-8'>
-              <p className="text-center w-full text-[24px] font-bold text-[#555555] font-serif"><span className="text-[#FF8D50]">《 </span>關心在地 <span className="text-[#FF8D50]">》</span></p>
-              <p className='text-[#555555] text-[14px] mt-4'>致力環境保護，承諾將積極減少溫室氣體排放、推動資源循環利用、減少廢棄物的產生，力求企業的永續發展及為環境保護盡一份力。</p>
+            <div className="absolute shadow-xl w-full left-0 group-hover:pb-12 top-0 duration-300 group-hover:translate-y-[80%] pt-[20%] rounded-b-[30px] px-8 z-10 bg-white">
+              <p className="text-center w-full text-[24px] font-bold text-[#555555] font-serif tracking-[2px]">
+                <span className="text-[#FF8D50]">《 </span>關心在地{" "}
+                <span className="text-[#FF8D50]">》</span>
+              </p>
+              <p className="text-[#555555] text-[14px] mt-4 leading-[28px] tracking-[1px]">
+                致力環境保護，承諾將積極減少溫室氣體排放、推動資源循環利用、減少廢棄物的產生，力求企業的永續發展及為環境保護盡一份力。
+              </p>
             </div>
           </div>
-          <div className='relative group'>
-            <div className=" rounded-[30px]  py-8 px-8 h-[100%] glow-card relative overflow-hidden min-h-[240px] duration-300 hover:shadow-xl hover:scale-[1.04] z-10" style={{ backgroundImage: `url(${img_card02})`, backgroundSize: 'cover',  }}>
+          <div className="relative group">
+            <div
+              className=" rounded-[30px]  py-8 px-8 h-[100%] glow-card relative overflow-hidden min-h-[240px] duration-300 hover:shadow-xl hover:scale-[1.04] z-20"
+              style={{
+                backgroundImage: `url(${img_card02})`,
+                backgroundSize: "cover",
+              }}
+            >
               <div className="w-[100%] h-[100%] z-10 absolute bg-black/30 left-0 top-0 rounded-[30px]"></div>
-              <p className="absolute text-white z-20 font-serif text-[90px] -right-1 -bottom-4">培<br />力</p>
+              <p className="absolute text-white z-20 font-serif text-[90px] -right-1 -bottom-4">
+                培<br />力
+              </p>
             </div>
 
-            <div className='absolute shadow-xl w-full left-0 top-0 group-hover:pb-12  duration-300 group-hover:translate-y-[80%] pt-[15%] rounded-b-[30px] px-8'>
-              <p className="text-center w-full text-[24px] font-bold text-[#555555] font-serif"><span className="text-[#FF8D50]">《 </span>關懷培力 <span className="text-[#FF8D50]">》</span></p>
-              <p className='text-[#555555] text-[14px] mt-4'>注重人權規範及員工權益，承諾創建一個對等、公平以及健康的工作環境，建立有效溝通管道，聆聽基層意見，並提供多元化培訓增進員工價值。社會方面將公益熱忱轉化為實際行動，持續投入時間和資源以支持社會公益項目，將溫馨關懷回饋於社會。</p>
+            <div className="absolute shadow-xl w-full left-0 top-0 group-hover:pb-12  duration-300 group-hover:translate-y-[80%] pt-[15%] rounded-b-[30px] px-8 z-10 bg-white">
+              <p className="text-center w-full text-[24px] font-bold text-[#555555] font-serif  tracking-[2px]">
+                <span className="text-[#FF8D50]">《 </span>關懷培力{" "}
+                <span className="text-[#FF8D50]">》</span>
+              </p>
+              <p className="text-[#555555] text-[14px] mt-4 leading-[28px] tracking-[1px]">
+                注重人權規範及員工權益，承諾創建一個對等、公平以及健康的工作環境，建立有效溝通管道，聆聽基層意見，並提供多元化培訓增進員工價值。社會方面將公益熱忱轉化為實際行動，持續投入時間和資源以支持社會公益項目，將溫馨關懷回饋於社會。
+              </p>
             </div>
           </div>
-          <div className='relative group'>
-            <div className=" rounded-[30px]  py-8 px-8 h-[100%] glow-card relative overflow-hidden min-h-[240px] duration-300 hover:shadow-xl hover:scale-[1.04] z-10" style={{ backgroundImage: `url(${img_card03})`, backgroundSize: 'cover',  }}>
+          <div className="relative group ">
+            <div
+              className=" rounded-[30px]  py-8 px-8 h-[100%] glow-card relative overflow-hidden min-h-[240px] duration-300 hover:shadow-xl hover:scale-[1.04] z-20"
+              style={{
+                backgroundImage: `url(${img_card03})`,
+                backgroundSize: "cover",
+              }}
+            >
               <div className="w-[100%] h-[100%] z-10 absolute bg-black/30 left-0 top-0 rounded-[30px]"></div>
-              <p className="absolute text-white z-20 font-serif text-[90px] -right-1 -bottom-4">根<br />基</p>
+              <p className="absolute text-white z-20 font-serif text-[90px] -right-1 -bottom-4">
+                根<br />基
+              </p>
             </div>
 
-            <div className='absolute shadow-xl w-full left-0 top-0  duration-300 group-hover:translate-y-[80%] group-hover:pb-12 pt-[15%] rounded-b-[30px] px-8'>
-              <p className="text-center w-full text-[24px] font-bold text-[#555555] font-serif"><span className="text-[#FF8D50]">《 </span>關注根基 <span className="text-[#FF8D50]">》</span></p>
-              <p className='text-[#555555] text-[14px] mt-4'>謹慎落實風險管理，靈活投資策略，確保績效提升，穩定公司營運根基。同時遵守商業行為有關法令，維護相關利害關係人權益。 致力實現經濟、社會和環境之間的平衡，承諾在創造價值的同時兼顧永續發展，為員工、合作夥伴、社區帶來正面的影響。</p>
+            <div className="absolute shadow-xl w-full left-0 top-0  duration-300 group-hover:translate-y-[80%] group-hover:pb-12 pt-[15%] rounded-b-[30px] px-8 z-10 bg-white">
+              <p className="text-center w-full text-[24px] font-bold text-[#555555] font-serif tracking-[2px]">
+                <span className="text-[#FF8D50]">《 </span>關注根基{" "}
+                <span className="text-[#FF8D50]">》</span>
+              </p>
+              <p className="text-[#555555] text-[14px] mt-4 leading-[28px] tracking-[1px]">
+                謹慎落實風險管理，靈活投資策略，確保績效提升，穩定公司營運根基。同時遵守商業行為有關法令，維護相關利害關係人權益。
+                致力實現經濟、社會和環境之間的平衡，承諾在創造價值的同時兼顧永續發展，為員工、合作夥伴、社區帶來正面的影響。
+              </p>
             </div>
           </div>
+        </div>
+      </div>
 
-        
+      <div className='bg-gradient-to-r from-[#19734E] to-[#3C9180] mt-[300px]'>
+        <div className="container mx-auto text-white">
+          <p className="text-[52px] pt-16 font-semibold">永續成果</p>
         </div>
       </div>
     </div>
